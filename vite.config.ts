88d15a0d104e44fd5e59ebad780b415c88b3339a
roco55 <rocoyuka30@gmail.com>
2024-11-18 23:48:38 +0200

    project setup

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@modules': path.resolve(__dirname, './src/modules'),
+      '@theme': path.resolve(__dirname, './src/theme'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@icons': path.resolve(__dirname, './src/icons'),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
